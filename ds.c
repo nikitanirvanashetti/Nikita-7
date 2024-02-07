69a73fe43ca1b9ad78d709b0cec77383af3b02d9
Nikita Nirvanashetti <nirvanashettinikita@gmail.com>
2024-02-07 19:38:59 +0530

    commit the create function

---
+++ ds.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int n,a[50];
+
+void create()
+{
+int i;
+printf("enter the value of n\n:");
+scanf("%d"&n);
+printf("enter %d array elements\n",n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+}
