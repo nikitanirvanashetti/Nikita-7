e97acc12d92b47fe88fa36de9e8480ab2483d647
nikitanirvanashetti <nirvanashettinikita@gmail.com>
2024-03-20 13:01:41 +0530

    commit d changes

--- test/test.c
+++ test/test.c
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 ncdkfdskalfdfjklsdfkthis is test.c file in the new dir
+=======
+thsu fgijerknej is crearte4df in nikita dir this is test.c file in the new dir
+>>>>>>> Stashed changes
